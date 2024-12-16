828ce59f7e1bb47e6ab3ebefe5d4cdcc1c90c863
Taeyong Jeon <taeyongjeon24@gmail.com>
2024-12-16 22:07:24 +0900

    Update __init__.py

--- {{cookiecutter.project_name}}/src/{{cookiecutter.project_name}}/__init__.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.project_name}}/__init__.py
@@ -1,3 +1,8 @@
 from .logging_config import setup_logging
 
+### typeguard setup ########################
+# from typeguard import install_import_hook
+# install_import_hook("src")
+############################################
+
 setup_logging()
