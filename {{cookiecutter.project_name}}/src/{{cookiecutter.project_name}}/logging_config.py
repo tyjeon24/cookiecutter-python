327f413c69557a20477321d623d7c54ff5aa3f9e
Taeyong Jeon <taeyongjeon24@gmail.com>
2024-12-18 21:30:50 +0900

    Update logging_config.py to add FileHandler

--- {{cookiecutter.project_name}}/src/{{cookiecutter.project_name}}/logging_config.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.project_name}}/logging_config.py
@@ -1,25 +1,39 @@
 import logging
 import logging.config
+from pathlib import Path
 
 
-def setup_logging():
+def setup_logging(log_file_path: str = ""):
+    if len(log_file_path) == 0:
+        log_file_path += f"{project_name}.log"
+
     logging_config = {
         "version": 1,
         "disable_existing_loggers": False,
-        "formatters": {
-            "standard": {"format": "%(asctime)s - %(levelname)s - %(message)s"},
-        },
+        "formatters": {"standard": {"format": "%(asctime)s - %(name)s - %(levelname)-8s - %(message)s"}},
         "handlers": {
             "console": {
-                "level": "INFO",
+                "level": "DEBUG",
                 "class": "logging.StreamHandler",
                 "formatter": "standard",
             },
+            "file": {
+                "level": "DEBUG",
+                "class": "logging.FileHandler",
+                "formatter": "standard",
+                "filename": log_file_path,
+                "mode": "a",
+            },
         },
         "root": {
-            "handlers": ["console"],
-            "level": "INFO",
+            "handlers": ["console", "file"],
+            "level": "DEBUG",
+            "propagate": False,
         },
     }
 
     logging.config.dictConfig(logging_config)
+
+
+project_name = Path().absolute().parent.name
+logger = logging.getLogger(project_name)
