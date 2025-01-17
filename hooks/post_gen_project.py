98174d2b7ccc8503624ad235dae25d10c90dc7f5
Taeyong Jeon <taeyongjeon24@gmail.com>
2025-01-17 05:24:38 +0000

    Add encoding params

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -9,7 +9,10 @@ def update_pyproject():
     toml_from = COOKIECUTTER_ROOT / Path("pyproject.toml")
     toml_to = ROOT / Path("pyproject.toml")
 
-    toml_to.write_text(toml_to.read_text() + toml_from.read_text())
+    toml_to.write_text(
+        toml_to.read_text(encoding="utf-8") + toml_from.read_text(encoding="utf-8"),
+        encoding="utf-8",
+    )
     print(f"[Update] {toml_to.absolute()}")
 
 
@@ -20,12 +23,12 @@ def setup_logging_config():
 
     init_from = config_dir_cookiecutter / Path("__init__.py")
     init_to = config_dir / Path("__init__.py")
-    init_to.write_text(init_from.read_text())
+    init_to.write_text(init_from.read_text(encoding="utf-8"), encoding="utf-8")
     print(f"[Add] {init_to.absolute()}")
 
     logging_from = config_dir_cookiecutter / Path("logging_config.py")
     logging_to = config_dir / Path("logging_config.py")
-    logging_to.write_text(logging_from.read_text())
+    logging_to.write_text(logging_from.read_text(encoding="utf-8"), encoding="utf-8")
     print(f"[Add] {logging_to.absolute()}")
 
 
@@ -35,14 +38,16 @@ def setup_src_init():
 
     src_init_from = src_dir_cookiecutter / Path("__init__.py")
     src_init_to = src_dir / Path("__init__.py")
-    src_init_to.write_text(src_init_from.read_text())
+    src_init_to.write_text(src_init_from.read_text(encoding="utf-8"), encoding="utf-8")
     print(f"[Add] {src_init_to.absolute()}")
 
 
 def setup_pre_commit():
     pre_commit_from = COOKIECUTTER_ROOT / Path(".pre-commit-config.yaml")
     pre_commit_to = ROOT / Path(".pre-commit-config.yaml")
-    pre_commit_to.write_text(pre_commit_from.read_text())
+    pre_commit_to.write_text(
+        pre_commit_from.read_text(encoding="utf-8"), encoding="utf-8"
+    )
     print(f"[Add] {pre_commit_to.absolute()}")
 
 
